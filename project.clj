69407ed5b801c4a03def49f5b82142a065c99d60
Sergey Trofimov <sergey@trofimov.name>
2023-03-09 14:53:09 +0100

    (deps): Upgrade undertow "2.3.4.Final", zmap "1.3.26"

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "The Unlicense" :url "https://unlicense.org"}
 
   :dependencies [;; Delayed values in maps
-                 [com.github.strojure/zmap "1.3.18"]
+                 [com.github.strojure/zmap "1.3.26"]
                  ;; Request utils from undertow
-                 [io.undertow/undertow-core "2.3.0.Final"]]
+                 [io.undertow/undertow-core "2.3.4.Final"]]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
              :dev,,,,, {:dependencies [[ring/ring-defaults "0.3.4"]]
