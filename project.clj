a9ed53840533cb35fa970dc1426fdb0b3cde76d8
Sergey Trofimov <sergey@trofimov.name>
2023-03-14 17:10:22 +0100

    (chore): Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/ring-lib "1.2.0-44-alpha1"
+(defproject com.github.strojure/ring-lib "1.2.0-SNAPSHOT"
   :description "Opinionated implementations for Clojure ring handler."
   :url "https://github.com/strojure/ring-lib"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
